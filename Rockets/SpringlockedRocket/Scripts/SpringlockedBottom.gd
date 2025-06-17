7bc72c1634439f3560637b49d429893301321531
Ethan <hensley.ethan64@gmail.com>
2025-06-16 21:12:42 -0400

    Added screen shake on launch, added springlocked rocket

---
+++ Rockets/SpringlockedRocket/Scripts/SpringlockedBottom.gd
@@ -0,0 +1,11 @@
+extends UpgradeIcon
+
+func _on_pressed() -> void:
+	if Constants.research >= part.upgradeCost and part.unlockedBool and !part.boughtBool:
+		Constants.research -= part.upgradeCost
+		Constants[part.constantsName] = true
+		part.boughtBool = Constants[part.constantsName]
+		Constants.bottomParts.push_front(part)
+		$IconSprite.texture = part.boughtSprite
+		line_2D.default_color = 'fcf8fc'
+		$Select.play()
